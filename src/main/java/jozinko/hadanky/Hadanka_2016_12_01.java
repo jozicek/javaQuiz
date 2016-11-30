2a45affbe241d9b52500a93ee6532c3e88b8209b
Jozef Krcmarik <j.krcmarik@gmail.com>
2016-11-30 14:47:16 +0100

    hadanka - 1.12.2016

---
+++ src/main/java/jozinko/hadanky/Hadanka_2016_12_01.java
@@ -0,0 +1,26 @@
+package jozinko.hadanky;
+
+
+/**
+ * Vystup bude :
+ *
+ * 1.) true, true, false
+ * 2.) false, false, false
+ * 3.) false, true, false
+ * 4.) true, false, false
+ * 5.) compilation failed on line 3
+ * 6.) compilation failed on line 5
+ * 7.) runtime error on line 4
+ *
+ */
+public class Hadanka_2016_12_01 {
+
+  public static void main(String[] args) {
+    int[] a = {1,2,3,4};
+    String s = "abcd";
+    System.out.println(a instanceof Object); // #3
+    System.out.println(null instanceof Object); // #4
+    System.out.println(s instanceof Integer); // #5
+  }
+
+}
