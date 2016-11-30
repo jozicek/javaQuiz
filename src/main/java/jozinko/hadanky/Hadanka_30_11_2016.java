b8b11ab34b5d8d418d752fc9cf04a038478acb36
Jozef Krcmarik <j.krcmarik@gmail.com>
2016-11-30 10:02:25 +0100

    nova hadanka - 30.11.2016

---
+++ src/main/java/jozinko/hadanky/Hadanka_30_11_2016.java
@@ -0,0 +1,26 @@
+package jozinko.hadanky;
+
+
+/**
+ * Aky bude vystup ?
+ *
+ * 1.) String impl
+ * 2.) Object impl
+ * 3.) Compilation failed
+ * 4.) Runtime error
+ */
+public class Hadanka_30_11_2016 {
+
+  public static void method(String s){
+    System.out.println("String impl");
+  }
+
+  public static void method(Object o){
+    System.out.println("Object impl");
+  }
+
+  public static void main(String[] args) {
+    method(null);
+  }
+
+}
