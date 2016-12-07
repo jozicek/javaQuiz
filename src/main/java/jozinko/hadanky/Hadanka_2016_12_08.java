de097a41f79f62b89b87202dfa3fbf69d9ca2e27
Jozef Krcmarik <j.krcmarik@gmail.com>
2016-12-07 12:37:14 +0100

    hadanka - 8.12.2016

---
+++ src/main/java/jozinko/hadanky/Hadanka_2016_12_08.java
@@ -0,0 +1,18 @@
+package jozinko.hadanky;
+
+/**
+ * 1.) 0
+ * 2.) 5
+ * 3.) compilation failed.
+ * 4.) NullPointerException
+ */
+public class Hadanka_2016_12_08 {
+
+  static int i=5;
+
+  public static void main(String[] args) {
+    Hadanka_2016_12_08 hadanka = null;
+    System.out.println("i value is "+hadanka.i);
+  }
+
+}
