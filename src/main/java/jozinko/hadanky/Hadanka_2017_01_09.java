5998ff46b7855e3d88a12f5b7bb5c8705d2d489e
Jozef Krcmarik <j.krcmarik@gmail.com>
2017-01-09 08:27:06 +0100

    hadanka - 9.1.2017

---
+++ src/main/java/jozinko/hadanky/Hadanka_2017_01_09.java
@@ -0,0 +1,22 @@
+package jozinko.hadanky;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1.) ok
+ * 2.) compilation failed on line 1
+ * 3.) compilation failed on line 3
+ * 4.) runtime error
+ */
+public class Hadanka_2017_01_09 {
+
+  private static final Map map = new HashMap();
+
+  public static void main(String[] args) {
+    map.put("param1", "value1"); //line 1
+    map.put("param2", "value2");
+    map = new HashMap(); // line 3
+  }
+
+}
