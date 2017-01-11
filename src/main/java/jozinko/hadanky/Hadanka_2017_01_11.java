472cb2af1fc88bcce9529210e258c717737d3afe
Jozef Krcmarik <j.krcmarik@gmail.com>
2017-01-11 07:17:52 +0100

    hadanka - 11.1.2017

---
+++ src/main/java/jozinko/hadanky/Hadanka_2017_01_11.java
@@ -0,0 +1,19 @@
+package jozinko.hadanky;
+
+/**
+ * 1.) true, true
+ * 2.) false, false
+ * 3.) false, true
+ * 4.) runtime error
+ * 5.) compilation failed
+ */
+public class Hadanka_2017_01_11 {
+
+  public static void main(String[] args) {
+    Integer a = 1000, b = 1000;
+    Integer c = 100, d = 100;
+    System.out.println(a == b);
+    System.out.println(c == d);
+  }
+
+}
