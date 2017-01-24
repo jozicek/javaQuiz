3329f21d772946e7eac25d67ee5f8b4d1f608b96
Jozef Krcmarik <j.krcmarik@gmail.com>
2017-01-24 10:27:26 +0100

    hadanka - 24.01.2017

---
+++ src/main/java/jozinko/hadanky/Hadanka_2017_01_24.java
@@ -0,0 +1,15 @@
+package jozinko.hadanky;
+
+/**
+ * 1.) valid
+ * 2.) compilation failed
+ * 3.) runtime error
+ */
+public class Hadanka_2017_01_24 {
+
+  public static void main(String[] args) {
+    http://www.google.com
+    System.out.println("valid");
+  }
+
+}
