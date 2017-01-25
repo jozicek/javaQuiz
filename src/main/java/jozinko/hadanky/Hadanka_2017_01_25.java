353a182d31e5594f15e8baecf8e2c64293113cb9
Jozef Krcmarik <j.krcmarik@gmail.com>
2017-01-25 09:06:03 +0100

    hadanka - 25.01.2017

---
+++ src/main/java/jozinko/hadanky/Hadanka_2017_01_25.java
@@ -0,0 +1,27 @@
+package jozinko.hadanky;
+
+/**
+ * 1.) ABCD
+ * 2.) Compilation fails.
+ * 3.) C
+ * 4.) BC
+ */
+public class Hadanka_2017_01_25 {
+
+  public static void main(String[] args) {
+    try {
+      badMethod();
+      System.out.print("A");
+    } catch (Exception ex) {
+      System.out.print("B");
+    } finally {
+      System.out.print("C");
+    }
+    System.out.print("D");
+  }
+
+  public static void badMethod() {
+    throw new Error();
+  }
+
+}
