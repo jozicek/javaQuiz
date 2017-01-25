d6c779e7cfb1a63d496eeee69eae75db72ef8e1d
Jozef Krcmarik <j.krcmarik@gmail.com>
2017-01-25 16:52:17 +0100

    hadanka - 26.1.2017

---
+++ src/main/java/jozinko/hadanky/Hadanka_2017_01_26.java
@@ -0,0 +1,19 @@
+package jozinko.hadanky;
+
+/**
+ *
+ */
+public class Hadanka_2017_01_26 {
+  static int index = 0;
+
+  public static void main(String args[]) {
+    System.out.println(test());
+  }
+
+  int test() {
+    int index = 1;
+    return index;
+  }
+
+
+}
