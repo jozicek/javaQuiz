c33fab38fc56e046d89729e46340d4c8b4ca6d36
Jozef Krcmarik <j.krcmarik@gmail.com>
2017-01-31 09:14:59 +0100

    hadanka - 31.1.2017

---
+++ src/main/java/jozinko/hadanky/Hadanka_2017_01_31.java
@@ -0,0 +1,24 @@
+package jozinko.hadanky;
+
+/**
+ * 1.) compilation ok
+ * 2.) out:4
+ * 3.) out:6
+ * 4.) compilation failed on line 1
+ * 5.) compilation failed on line 2
+ */
+public class Hadanka_2017_01_31 {
+
+  private int x = 4;
+
+  public static void main(String[] args) {
+    protected int x = 6; // #1
+    new Hadanka_2017_01_31().new Inner().out(); // #2
+  }
+
+  class Inner {
+    void out() {
+      System.out.println("out:" + x);
+    }
+  }
+}
